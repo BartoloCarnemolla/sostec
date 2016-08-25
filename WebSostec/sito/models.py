24180dd33c798bbf7beeb5ed633fc00c7c7ed4ca
Bartolo Carnemolla <bartolo.carnemolla@gmail.com>
2016-08-25 17:57:53 +0200

    Configurazione editor PyCharm

--- WebSostec/sito/models.py
+++ WebSostec/sito/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    nome = models.CharField(max_length=40)
+    email = models.CharField(max_length=100)
+    titolo = models.CharField(max_length=100)
+    immagine = models.CharField(max_length=200, default='no')
+
+    def __str__(self):
+        return self.nome
